151bebdc64cd61372002429e1293765ebcb3a4bd
Cedric Morgantini <c.morgantini@free.fr>
2021-12-18 13:05:57 +0100

    ajout fichier test pour point-updates-are-not-reflected

--- tests/integration/test_book.py
+++ tests/integration/test_book.py
@@ -15,3 +15,16 @@ def test_booking_place_in_past_competition_must_be_impossible(client):
 
     assert flash_message in data
     assert response.status_code == 403
+
+
+def test_after_booking_points_available_are_reduced(client):
+    """ vérifie qu'une fois la réservation effectuée, les points disponibles
+     aient bien été diminués du tarif de la réservation."""
+    datas = {'club': 'Simply Lift', 'competition': 'Spring Festival', 'places': '5'}
+    response = client.post('/purchasePlaces', data=datas, follow_redirects=True)
+    data = response.data.decode()
+    # Après déduction des 5 places sur les 7 points, le template doit contenir :
+    expected_result = "Points available: 8"
+
+    assert expected_result in data
+    assert response.status_code == 200

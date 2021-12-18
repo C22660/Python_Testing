c274b35785c4ce775a0cca09736ce2db2c884c57
Cedric Morgantini <c.morgantini@free.fr>
2021-12-18 13:47:33 +0100

    ajout fichier test no-use-more-than-points-allowed

--- tests/integration/test_book.py
+++ tests/integration/test_book.py
@@ -41,3 +41,24 @@ def test_not_possible_to_book_more_than_twelve_places(client):
 
     assert flash_message in data
     assert response.status_code == 403
+
+
+def test_not_possible_to_book_more_than_points_available(client, mocker):
+    """ vérifie quil n'est pas possible de réserver plus de places que
+     de points disponibles pour le club."""
+    # création d'un club avec un nombre de place faible :
+    fake_club = [
+        {
+            "name": "Simply Lift",
+            "email": "john@simplylift.co",
+            "points": "3"
+        }]
+    # remplacement des données du json par ce club modifié
+    mocker.patch.object(server, 'clubs', fake_club)
+    datas = {'club': 'Simply Lift', 'competition': 'Spring Festival', 'places': '10'}
+    flash_message = "Sorry, you have only 3 points."
+    response = client.post('/purchasePlaces', data=datas, follow_redirects=True)
+    data = response.data.decode()
+
+    assert flash_message in data
+    assert response.status_code == 403

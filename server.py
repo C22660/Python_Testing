c9503a319d06b9590681d506977822a9299f220b
Cedric Morgantini <c.morgantini@free.fr>
2021-12-18 13:06:57 +0100

    ajout dans server correctif pour point-updates-are-not-reflected

--- server.py
+++ server.py
@@ -68,9 +68,19 @@ def create_app():
         competition = [c for c in competitions if c['name'] == request.form['competition']][0]
         club = [c for c in clubs if c['name'] == request.form['club']][0]
         places_required = int(request.form['places'])
-        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-places_required
-        flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
+        today = str(datetime.now())
+        if competition["date"] < today:
+            flash('Sorry, this event is already passed. Impossible to book it')
+            return render_template('welcome.html', club=club, competitions=competitions), 403
+        else:
+            if places_required > 12:
+                flash("Sorry, it's not possible to book more than 12 places.")
+                return render_template('booking.html', club=club, competition=competition), 403
+            else:
+                competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+                club['points'] = int(club['points']) - places_required
+                flash('Great-booking succeded!')
+                return render_template('welcome.html', club=club, competitions=competitions)
 
     # TODO: Add route for points display
 
